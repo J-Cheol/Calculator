e1ce2c29c469081080101420c083caaee7b81d93
J-Cheol <lunaris0206@naver.com>
2023-10-30 10:03:54 +0900

    step 1 : str -> toCharArray

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,8 +2,38 @@ package com.ll;
 
 public class Calc
 {
-    public void run(String sum)
+    public static int run(String calc)
     {
+        char[] chars = calc.toCharArray();
+        int num1 = 0;
+        int num2 = 0;
+        char operator = ' ';
+        for (char c : chars)
+        {
+            if (c >= '0' && c <= '9')
+            {
+                if(num1 == 0)
+                    num1 = c - '0';
+                else
+                {
+                    num2 = c - '0';
+                }
+            } else if (c == '*' || c == '/' || c == '%' || c == '+' || c == '-')
+                operator = c;
+        }
 
+        int result = 0;
+        if (operator == '*')
+            result = num1 * num2;
+        if (operator == '/')
+            result = num1 / num2;
+        if (operator == '%')
+            result = num1 % num2;
+        if (operator == '-')
+            result = num1 - num2;
+        if (operator == '+')
+            result = num1 + num2;
+
+        return result;
     }
 }
--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -4,6 +4,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        new Calc().run("3 * 1");
+
+        System.out.println(Calc.run("3 * 1"));
     }
 }
